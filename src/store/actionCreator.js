45f8b01cbe87be59453e98dc7503fd6a76df6c33
shiyi <duanlijun@dhcc.com.cn>
2020-08-13 15:59:56 +0800

    dev -测试

--- src/store/actionCreator.js
+++ src/store/actionCreator.js
@@ -1,5 +1,5 @@
-import { CHANGE_INPUT_VALUE, CHANGE_LIST_VALUE } from './actionType'
-
+import { CHANGE_INPUT_VALUE, CHANGE_LIST_VALUE, INIT_LIST_VALUE } from './actionType'
+import axios from 'axios'
 export const getInputChangeAction = ((value) => ({
     type: CHANGE_INPUT_VALUE,
     value
@@ -9,3 +9,21 @@ export const getListChangeAction = ((value) => ({
     type: CHANGE_LIST_VALUE,
     value
 }));
+
+export const getInitListValue = () => {
+    return ((dispatch) => {
+        /*axios.get('/api/js').then((res) => {
+            alert('ok');
+        }).catch((err) => {
+            alert('err');
+        })*/
+        setTimeout(function () {
+            const data = ['aaa', 'bbb', 'ccc', 'ddd', 'eee'];
+            const action = {
+                type: INIT_LIST_VALUE,
+                value: data
+            }
+            dispatch(action)
+        }, 500)
+    })
+}
